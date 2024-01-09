06b6d499213295a28884bcb7157e8efe90e8e8e9
Pedro Santana <123907570+VersoKZ@users.noreply.github.com>
2024-01-09 02:32:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const imagens = document.querySelector('#imagem')
+
+function primeiroA() {
+    imagens.innerHTML = "<img src='imagens/primeiroA.png'>"
+    imagens.classList.add('imagens')
+}
+
+function primeiroB() {
+    imagens.innerHTML = "<img src='imagens/primeiroB.png'>"
+}
+
+function primeiroC() {
+    imagens.innerHTML = "<img src= 'imagens/primeiroC.png'>"
+
+}
+
+function primeiroD() {
+    imagens.innerHTML = "<img src= 'imagens/primeiroD.png'>"
+}
+
+function segundoA() {
+    imagens.innerHTML = "<img src= 'imagens/segundoA.png'>"
+}
+
+function segundoB() {
+    imagens.innerHTML = "<img src= 'imagens/segundoB.png'>"
+}
+
+function segundoC() {
+    imagens.innerHTML = "<img src= 'imagens/segundoC.png'>"
+}
+
+function segundoD() {
+    imagens.innerHTML = "<img src= 'imagens/segundoD.png'>"
+}
+
+function terceiroA() {
+    imagens.innerHTML = "<img src= 'imagens/terceiroA.png'>"
+}
+
+function terceiroB() {
+    imagens.innerHTML = "<img src= 'imagens/terceiroB.png'>"
+}
+
+function terceiroC() {
+    imagens.innerHTML = "<img src= 'imagens/terceiroC.png'>"
+}
+
+function terceiroD() {
+    imagens.innerHTML = "<img src= 'imagens/terceiroD.png'>"
+}
+
